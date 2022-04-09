cd809fda50c14e37f8278ebf6e4321edc4eef143
Vaishnavi Dontineni <vaishd@cs.washington.edu>
2022-04-09 15:30:14 -0700

    get search results from amazon

--- popup.js
+++ popup.js
@@ -1,25 +1,22 @@
 let locationName = ''
-let chosenLat = 0;
-let chosenLong = 0;
 var geolocation_api_key = config.GEO_LOCATION_API_KEY;
 
 document.addEventListener('DOMContentLoaded', function () {
 
     navigator.geolocation.getCurrentPosition(position => {
         const { latitude, longitude } = position.coords;
-        const getCityAPILink = 'https://www.mapquestapi.com/geocoding/v1/reverse?key=' + geolocation_key + '&location=' + latitude + '%2C' + longitude + '&outFormat=json&thumbMaps=false';
+        const getCityAPILink = 'https://www.mapquestapi.com/geocoding/v1/reverse?key=' + geolocation_api_key + '&location=' + latitude + '%2C' + longitude + '&outFormat=json&thumbMaps=false';
 
         // getting city from coordinates
         fetch(getCityAPILink).then(r => r.text()).then(result => {
             let addressJSON = JSON.parse([result])
             locationName = addressJSON.results[0].locations[0].adminArea5 + ", " + addressJSON.results[0].locations[0].adminArea3;
-            console.log(locationName);
-
         })
-
+        
         chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
-            console.log(tabs[0].url);
-            console.log(tabs[0].title);
+            let start = tabs[0].url.indexOf("s?k=") + 4;
+            let end = tabs[0].url.indexOf("&");
+            console.log(tabs[0].url.substring(start, end));
           });
     });
 }, false);
