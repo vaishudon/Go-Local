57db75354779bca46dfb5cbad9c3a1df1de31cfa
crajas@cs.washington.edu <chandnir02@gmail.com>
2022-04-09 21:22:23 -0700

    implement location search

--- popup.js
+++ popup.js
@@ -1,38 +1,45 @@
 let locationName = ''
+let geocode = "";
 var geolocation_api_key = config.GEO_LOCATION_API_KEY;
 
 document.addEventListener('DOMContentLoaded', function () {
 
-    navigator.geolocation.getCurrentPosition(position => {
-        const { latitude, longitude } = position.coords;
-        const getCityAPILink = 'https://www.mapquestapi.com/geocoding/v1/reverse?key=' + geolocation_api_key + '&location=' + latitude + '%2C' + longitude + '&outFormat=json&thumbMaps=false';
-
-        // getting city from coordinates
-        fetch(getCityAPILink).then(r => r.text()).then(result => {
-            let addressJSON = JSON.parse([result])
-            locationName = addressJSON.results[0].locations[0].adminArea5 + ", " + addressJSON.results[0].locations[0].adminArea3;
-         //  console.log(locationName);
-
-        })
-    });
-
     chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
-        console.log(tabs[0].url);
-        console.log(tabs[0].title);
-
         let start = tabs[0].url.indexOf("s?k=") + 4;
         let end = tabs[0].url.indexOf("&");
         let product_name = tabs[0].url.substring(start, end);
 
-        fetch('http://127.0.0.1:5000/retrieve?product=' + product_name)
-            .then((response) => response.json())
-            .then(displayProducts)
-            .catch(console.err);
+        navigator.geolocation.getCurrentPosition(position => {
+            const { latitude, longitude } = position.coords;
+            const getCityAPILink = 'https://www.mapquestapi.com/geocoding/v1/reverse?key=' + geolocation_api_key + '&location=' + latitude + '%2C' + longitude + '&outFormat=json&thumbMaps=false';
 
-        console.log(product_name);
+            // getting city from coordinates
+            fetch(getCityAPILink).then(r => r.text()).then(result => {
+                let addressJSON = JSON.parse([result])
+                locationName = addressJSON.results[0].locations[0].adminArea5;
+                console.log(locationName);
+            }).then(()=>getGeocode(product_name))
+        });
       });
 
 }, false);
+
+function getGeocode(product_name) {
+    fetch("./IP2LOCATION-GEONAMEID.json")
+    .then(response => { return response.json();})
+    .then(jsondata => {return jsondata[locationName][0]["geonameid"];})
+    .then((code)=>getProducts(product_name, code));
+}
+
+function getProducts(product_name, geocode) {
+    console.log(product_name)
+    console.log(geocode)
+    fetch('http://127.0.0.1:5000/retrieve?product=' + product_name + "&locationQuery=" + geocode)
+    .then((response) => response.json())
+    .then(displayProducts)
+    .catch(console.err);
+
+}
 
 function displayProducts(response) {
     let section = document.querySelector("#items");
